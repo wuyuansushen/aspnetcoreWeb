3e01bf38330bb8e28e1dd737dc5c36ab10e3731e
wuyuansushen <819070864@qq.com>
2020-12-09 14:48:15 +0800

    Add project files.

---
+++ IpReflection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace aspnetcoreWeb
+{
+    public class IpReflection : IIpReflection
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+        public IpReflection(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public XForward GetIp()
+        {
+            var headerPairs = _httpContextAccessor.HttpContext.Request.Headers;
+            string XForIP = headerPairs["X-Forwarded-For"];
+            if (XForIP == null)
+                XForIP = "Null";
+            else
+                XForIP = (XForIP.Split(','))[0];
+            XForward forJson = new XForward() { ip = XForIP };
+            return forJson;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace aspnetcoreWeb
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            services.AddTransient<IIpReflection, IpReflection>();
+            services.AddDirectoryBrowser();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,IIpReflection ipInfo)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            string locationPath = env.ContentRootPath + @"/store";
+            var fileLocation = new PhysicalFileProvider(locationPath);
+            app.UseStaticFiles(new StaticFileOptions() { RequestPath = "/ftp", FileProvider=fileLocation });
+            app.UseDirectoryBrowser(options:(new DirectoryBrowserOptions() { RequestPath="/ftp",FileProvider=fileLocation}));
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/ip", async context =>
+                {
+                    var XForInfo = ipInfo.GetIp();
+                    var jsonOut = JsonSerializer.Serialize<XForward>(XForInfo,options:(new JsonSerializerOptions{WriteIndented=true}));
+                    await context.Response.WriteAsync(jsonOut);
+                });
+            });
+        }
+    }
+}
